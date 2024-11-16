1a748086ce9af5798779cf4b20476d64eff735a0
Peter Heyboer <heyboer.peter@gmail.com>
2024-11-15 20:48:10 -0500

    adding comments for clarity

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,14 +3,17 @@
 $(document).ready(function() {
   console.log("composer-char-counter.js is loaded!");
 
+  // attach input event handler to textarea
   $(".new-tweet textarea").on("input", function() {
     const maxChars = 140;
     const currentLength = $(this).val().length;
     const remainChars = maxChars - currentLength;
 
+    // target the counter through DOM
     const counter = $(this).closest(".new-tweet").find(".counter");
     counter.text(remainChars);
 
+    // change color of counter when over limit by adding class and styling
     if (remainChars < 0) {
       counter.addClass("max-limit");
     } else {
