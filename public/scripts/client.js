eec1d96d7d147ba2a87ccbd1af36271b6526ebe4
Peter Heyboer <heyboer.peter@gmail.com>
2024-11-19 14:24:34 -0500

    adding and testing data for dynamic tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,27 +4,40 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+'use strict';
+
+
 // Function createTweetElement takes in a tweet object and returns tweet article
 // element containing html structure of tweet
 // object taken from initial-tweets.json
 // function should return $tweet to the caller
-
-'use strict';
+// Document ready function to ensure Javascript runs after HTML
 
 $(document).ready(function () {
   // Test / driver code (temporary). Eventually will get this from the server.
-  const tweetData = [
+  const tweetData =  [
     {
-      user: {
-        name: 'Newton',
-        avatars: 'https://i.imgur.com/73hZDYK.png',
-        handle: '@SirIsaac',
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
       },
-      content: {
-        text: 'If I have seen further it is by standing on the shoulders of giants',
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
       },
-      created_at: 1461116232227,
+      "created_at": 1461116232227
     },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
   ];
 
   // Function to turn tweet object into article element
@@ -55,7 +68,6 @@ $(document).ready(function () {
     return $tweet;
   };
 
-  // const $tweet = createTweetElement(tweetData);
 
   // Function to append tweet to #tweeet-container
   // loop through tweets
@@ -72,7 +84,3 @@ $(document).ready(function () {
 
   renderTweets(tweetData);
 });
-
-// // Test / driver code (temporary)
-// console.log($tweet); // to see what it looks like
-// $('#tweet-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
