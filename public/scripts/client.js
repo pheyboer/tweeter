7aa038bf58e0698e272e1e186d073430427ebc91
Peter Heyboer <heyboer.peter@gmail.com>
2024-11-19 11:24:19 -0500

    setting up dynamic tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,10 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Function createTweetElement takes in a tweet object and returns tweet article
+// element containing html structure of tweet
+// object taken from initial-tweets.json
+// function should return $tweet to the caller
+
+
+const $tweet = $(`<article class="tweet">Hello world</article>`);
