f118e4ccea709410fcde37f6263a0a8a2d6f47ea
Peter Heyboer <heyboer.peter@gmail.com>
2024-11-19 11:44:55 -0500

    setting up and implementing dynamic tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -9,5 +9,31 @@
 // object taken from initial-tweets.json
 // function should return $tweet to the caller
 
+// Test / driver code (temporary). Eventually will get this from the server.
+const tweetData = {
+  user: {
+    name: 'Newton',
+    avatars: 'https://i.imgur.com/73hZDYK.png',
+    handle: '@SirIsaac',
+  },
+  content: {
+    text: 'If I have seen further it is by standing on the shoulders of giants',
+  },
+  created_at: 1461116232227,
+};
 
-const $tweet = $(`<article class="tweet">Hello world</article>`);
+const createTweetElement = function (tweet) {
+  const $tweet = $(`
+    <article class="tweet">
+      <header>
+        <div class="">
+    </article>
+    `);
+
+  return $tweet;
+};
+
+const $tweet = createTweetElement(tweetData);
+// Test / driver code (temporary)
+console.log($tweet); // to see what it looks like
+$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
